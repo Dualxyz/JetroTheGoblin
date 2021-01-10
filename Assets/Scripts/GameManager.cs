fe803796bd9a9ac7ce58cfed4378472188a93232
Dual <shikesenpu@gmail.com>
2021-01-11 00:40:10 +0100

    Added ingame Menu placeholder

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject InventoryAll;
+    public GameObject EscMenu;
+    public bool MenuDisplay = false;
     public bool InventoryDisplay = false; //Idk why it displays an error here?:D
     // Start is called before the first frame update
     void Start()
@@ -19,8 +22,23 @@ public class GameManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.I) || Input.GetKeyDown(KeyCode.B)){
             showhidePanel();
         }
+
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            showhideMenu();
+        }
     }
 
+    public void showhideMenu(){
+         if(MenuDisplay == false){
+            EscMenu.gameObject.SetActive(true);
+            print("You have opened the Inventory.");
+            MenuDisplay = true;
+        } else {
+            EscMenu.gameObject.SetActive(false);
+            print("You have closed the Inventory.");
+            MenuDisplay = false;
+        }
+    }
     public void showhidePanel(){
         if(InventoryDisplay == false){
             InventoryAll.gameObject.SetActive(true);
@@ -31,5 +49,9 @@ public class GameManager : MonoBehaviour
             print("You have closed the Inventory.");
             InventoryDisplay = false;
         }
+    }
+
+    public void backToMainMenu(){
+        SceneManager.LoadScene("Main Menu");
     }
 }
