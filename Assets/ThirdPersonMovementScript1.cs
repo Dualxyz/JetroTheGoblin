50bc9362724d9a3023a2a3e571a98e65b6dd3284
Daniel Dias <56049583+danieldias99@users.noreply.github.com>
2021-02-06 23:39:13 +0000

    Add gravity to third person object

--- Assets/ThirdPersonMovementScript1.cs
+++ Assets/ThirdPersonMovementScript1.cs
@@ -4,62 +4,89 @@ using UnityEngine;
 
 public class ThirdPersonMovementScript1 : MonoBehaviour
 {
-    public CharacterController controller;  //motor that drtives our player
-    public Transform targetPosition;
-    public Rigidbody test;
+
+    //VARIABLES
+    [SerializeField] private float moveSpeed;
+
+    private Vector3 moveDirection; // keep track of movement while in Ground
+    private Vector3 velocity; // keep track of movement while jumping
+
+    [SerializeField] private bool isGrounded; // keep track if player is in Ground
+    [SerializeField] private float groundCheckDistance; //??
+    [SerializeField] private LayerMask groundMask; //??
+    [SerializeField] private float gravity; // value of gravity
+
+    //REFERENCES
+    private CharacterController controller;  //motor that drtives our player
+
+    //Camera
     public Transform cam;
-    public float speed = 6f;
     public float turnSmoothTime = 0.1f;
     private float turnSmoothVelocity;
-    [SerializeField] private float gravity = -9.81f;
-	public bool PlayerGravity = false;
+
+    private void Start()
+    {
+        this.controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        Move();
+
+    }
 
     // Update is called once per frame
-    void walk(){
+    private void Move()
+    {
+        GravityCheck();
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
 
-        if(direction.magnitude >= 0.1f){
+        if (direction.magnitude >= 0.1f)
+        {
             float targetAngle = Mathf.Atan2(-direction.x, -direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * -Vector3.forward;
-            controller.Move(moveDir.normalized*speed*Time.deltaTime);
-            
-            //targetPosition.position = Vector3.MoveTowards;
-            //controller.Move(*Time.deltaTime*gravity);
-            
+            controller.Move(moveDir.normalized * moveSpeed * Time.deltaTime);
+
+            ApplyGravityToPlayer();
         }
-        //direction.y = gravity;
-        //controller.Move();
+
     }
 
-    void Update()
+    private void GravityCheck()
     {
-        walk();
+        this.isGrounded = isInGround(transform.position);
 
-    }
-
-     public void SetGravity(){
-        if(PlayerGravity == false){
-        	PlayerGravity = true;
-           	GravityBoost();
-        } else {
-           PlayerGravity = false;
-           resetGravityBoost();
+        //stops apply gravity if player is in Ground
+        if (this.isGrounded && this.velocity.y < 0)
+        {
+            this.velocity.y = -2f;
         }
     }
 
-	public void GravityBoost(){
-		gravity = -20000f;
-		print("gravity set to: "+ gravity);
-	}
-//commentx2
-	public void resetGravityBoost(){
-		gravity = -9.81f;
-		print("gravity reset to: "+ gravity);
-	}
-    
+    /// <summary>
+    /// Checks if Object is in Ground based on its position transform.
+    /// </summary>
+    /// <param name="position">Transform containing position of Object</param>
+    /// <returns></returns>
+    private bool isInGround(Vector3 position)
+    {
+        return Physics.CheckSphere(position, this.groundCheckDistance, this.groundMask);
+    }
+
+    /// <summary>
+    /// Applys gravity to Player
+    /// </summary>
+    private void ApplyGravityToPlayer()
+    {
+        this.velocity.y += this.gravity * Time.deltaTime;
+        this.controller.Move(this.velocity * Time.deltaTime);
+    }
+
+
 }
